786a53a1b44f32ce8328fe6c4066d6203b131490
gloria <macuser@MacUsers-MacBook-Pro.local>
2022-07-12 11:31:43 -0400

    add logic for base, confirm, danger, click, disable

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,5 +3,18 @@ import React from "react";
 import "components/Button.scss";
 
 export default function Button(props) {
-   return <></>;
+   const {onClick, disabled, children} = props;
+
+   let buttonClass = "button";
+   if (props.confirm) {
+      buttonClass += " button--confirm";
+   } else if (props.danger) {
+      buttonClass += " button--danger";
+   }
+
+   return (
+      <button onClick={onClick} className={buttonClass} disabled={disabled}>
+         {children}
+      </button>
+   );
 }
