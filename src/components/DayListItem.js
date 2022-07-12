884b06ba36e2d3a51d3e65cb907e457bdc5c7407
gloria <macuser@MacUsers-MacBook-Pro.local>
2022-07-12 13:49:10 -0400

    add formatSpots func to output correct number

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -8,11 +8,21 @@ export default function DayListItem(props) {
   let dayClass = "day-list__item"
   dayClass += classNames({"--selected": props.selected});
   dayClass += classNames({"--full": props.spots === 0});
+
+  const formatSpots = function() {
+    if (props.spots === 0) {
+      return "no spots remaining";
+    } else if (props.spots === 1) {
+      return "1 spot remaining";
+    } else {
+      return `${props.spots} spots remaining`;
+    }
+  };
   
   return (
     <li onClick={()=>setDay(props.name)} className={dayClass}>
       <h2 className="text--regular">Day Name</h2> 
-      <h3 className="text--light">X spots remaining</h3>
+      <h3 className="text--light">{formatSpots()}</h3>
     </li>
   );
 }
