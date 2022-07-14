8ed93697161e46626607e9c866da37b25994f775
gloria <macuser@MacUsers-MBP.JOOWIN>
2022-07-14 12:10:34 -0400

    destructure props

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -3,26 +3,26 @@ import classNames from "classnames";
 import "components/DayListItem.scss";
 
 export default function DayListItem(props) {
-  const {setDay} = props;
+  const {onChange, selected, spots, name} = props;
 
   const dayClass = classNames("day-list__item", {
-    "day-list__item--selected": props.selected,
-    "day-list__item--full": props.spots === 0
+    "day-list__item--selected": selected,
+    "day-list__item--full": spots === 0
   });
 
   const formatSpots = function() {
-    if (props.spots === 0) {
+    if (spots === 0) {
       return "no spots remaining";
-    } else if (props.spots === 1) {
+    } else if (spots === 1) {
       return "1 spot remaining";
     } else {
-      return `${props.spots} spots remaining`;
+      return `${spots} spots remaining`;
     }
   };
   
   return (
-    <li onClick={setDay} className={dayClass}>
-      <h2 className="text--regular">{props.name}</h2> 
+    <li onClick={()=>onChange(name)} className={dayClass}>
+      <h2 className="text--regular">{name}</h2> 
       <h3 className="text--light">{formatSpots()}</h3>
     </li>
   );
