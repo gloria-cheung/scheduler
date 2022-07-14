422e8b2f8e210bb6128b19d73f035718f086a747
gloria <macuser@MacUsers-MBP.JOOWIN>
2022-07-14 15:15:58 -0400

    render header, show or empty appointment based on props

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,9 +2,17 @@ import React from "react";
 import classNames from "classnames";
 import "components/Appointment/styles.scss";
 
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
+
 export default function Appointment(props) {
-  
+  const {time, id, interview} = props
   return (
-    <article className="appointment"></article>
+    <article className="appointment">
+      <Header time={time}/>
+      {props.interview ? 
+        <Show {...interview} /> : <Empty />}
+    </article>
   );
 }
