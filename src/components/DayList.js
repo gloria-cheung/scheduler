88025b31030fda698f0de5d64e3487d7ef70d07c
gloria <macuser@MacUsers-MacBook-Pro.local>
2022-07-18 20:34:02 -0400

    fix bug by renaming day to value

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -2,14 +2,14 @@ import React from "react";
 import DayListItem from "./DayListItem";
 
 export default function DayList(props) {
-  const {days, onChange, day} = props;
+  const {days, onChange, value} = props;
   const daysData = days.map(d => {
     return (
       <DayListItem 
       {...d} 
       key={d.id} 
       onChange={onChange} 
-      selected={d.name === day} 
+      selected={d.name === value} 
       />
     )})
   return(
