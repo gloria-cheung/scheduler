537c745ef5d4ce522ae668fd594dfaf0013001fa
gloria <macuser@MacUsers-MacBook-Pro.local>
2022-07-21 11:06:40 -0400

    add semicolon

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -2,7 +2,7 @@ export function getAppointmentsForDay(state, day) {
   // return array of appointments for that day
   for (let eachDay of state.days) {
     if (eachDay.name === day) {
-       return eachDay.appointments.map(id => state.appointments[id])
+       return eachDay.appointments.map(id => state.appointments[id]);
     }
   }
   return [];
