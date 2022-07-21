6060ce6876fd59fdb208f2c1a314ee2a64761664
gloria <macuser@MacUsers-MacBook-Pro.local>
2022-07-21 11:06:07 -0400

    add helper func getAppointmentsForDay

---
+++ src/helpers/selectors.js
@@ -0,0 +1,9 @@
+export function getAppointmentsForDay(state, day) {
+  // return array of appointments for that day
+  for (let eachDay of state.days) {
+    if (eachDay.name === day) {
+       return eachDay.appointments.map(id => state.appointments[id])
+    }
+  }
+  return [];
+};
