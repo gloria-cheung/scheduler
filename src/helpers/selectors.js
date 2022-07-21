3976c705522789eb3cf4e166458efa2f7f594554
gloria <macuser@MacUsers-MacBook-Pro.local>
2022-07-21 13:16:53 -0400

    create getInterview func to return interview obj

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -7,3 +7,13 @@ export function getAppointmentsForDay(state, day) {
   }
   return [];
 };
+
+export function getInterview(state, interview) {
+  // return obj continaing interview data
+  if (interview) {
+    let interviewerId = interview.interviewer;
+    interview.interviewer = state.interviewers[interviewerId];
+    return interview;
+  }
+  return null;
+};
