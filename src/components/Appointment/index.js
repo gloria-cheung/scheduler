bd16a0ee6bd357ee00f53126b81e961845ee5e23
gloria <macuser@MacUsers-MacBook-Pro.local>
2022-07-25 20:58:13 -0400

    use bookInterview prop to make save func and create new interview obj

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -8,7 +8,7 @@ import Form from "./Form";
 import { useVisualMode } from "hooks/useVisualMode";
 
 export default function Appointment(props) {
-  const {time, interview, interviewers} = props;
+  const {id, time, interview, interviewers, bookInterview} = props;
   
   const EMPTY = "EMPTY";
   const SHOW = "SHOW";
@@ -17,6 +17,15 @@ export default function Appointment(props) {
   const {mode, transition, back} = useVisualMode(
     props.interview ? SHOW : EMPTY
   )
+  
+  const save = function(name, interviewer) {
+    const interview = {
+      student: name,
+      interviewer
+    };
+    bookInterview(id, interview);
+    transition(SHOW);
+  };
 
   return (
     <article className="appointment">
@@ -24,7 +33,12 @@ export default function Appointment(props) {
       <Header time={time}/>
       {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
       {mode === SHOW && <Show {...interview} />}
-      {mode === CREATE && <Form interviewers={interviewers} onCancel={() => back(EMPTY)}/>}
+      {mode === CREATE && <
+        Form 
+          interviewers={interviewers} 
+          onCancel={() => back(EMPTY)} 
+          save={save}
+        />}
 
     </article>
   );
