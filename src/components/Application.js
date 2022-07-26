6eb397e81442868c2ecb9db06ff14c109cce6906
gloria <macuser@MacUsers-MacBook-Pro.local>
2022-07-26 12:16:11 -0400

    refactor to move state logic to custom hook

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,74 +1,14 @@
-import React, {useState, useEffect} from "react";
+import React from "react";
 import DayList from "./DayList";
-import axios from "axios";
 
 import "components/Application.scss";
 import Appointment from "./Appointment";
 import { getAppointmentsForDay, getInterview, getInterviewersForDay } from "helpers/selectors";
+import { useApplicationData } from "hooks/useApplicationData";
 
 export default function Application() {
-  const [state, setState] = useState({
-    day: "Monday",
-    days: [],
-    appointments: {},
-    interviewers: {}
-  })
-
-  const setDay = day => setState({...state, day});
-
-  useEffect(() => {
-    Promise.all([
-      axios.get("/api/days"),
-      axios.get("/api/appointments"),
-      axios.get("/api/interviewers")
-    ]).then((all) => {
-      setState(prev => ({
-        ...prev, 
-        days: all[0].data, 
-        appointments: all[1].data,
-        interviewers: all[2].data
-      }));
-    });
-  }, []);
-
-  const bookInterview = async function(id, interview) {
-    const appointment = {
-      ...state.appointments[id],
-      interview: {...interview}
-    };
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    };
-
-    await axios.put(`/api/appointments/${id}`, {interview})
-    .then(result => {
-      setState({...state, appointments});
-    })
-    .catch(e => {
-      console.log(e);
-      throw e;
-    });
-  };
-
-  const cancelInterview = async function(id) {
-    const appointments = {
-      ...state.appointments, 
-      [id]: {
-        ...state.appointments[id], 
-        interview: null
-      }
-    }
-
-    await axios.delete(`/api/appointments/${id}`)
-      .then(result => {
-        setState({...state, appointments});
-      })
-      .catch(e => {
-        console.log(e);
-        throw e;
-      });
-  };
+  // state loading and actions moved to custom hook
+  const {state, setDay, bookInterview, cancelInterview} = useApplicationData();
 
   // using helper func to find appointments for single day
   const dailyAppointments = getAppointmentsForDay(state, state.day);
