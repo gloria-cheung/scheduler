d8d664989b9e0f721b204c561f5ec12df52bebf6
gloria <macuser@MacUsers-MacBook-Pro.local>
2022-08-02 10:32:19 -0400

    import proptypes and ensure interviewers prop is arr

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,8 +1,9 @@
 import React from "react";
 import InterviewListItem from "./InterviewerListItem";
 import "components/InterviewerList.scss";
+import PropTypes from 'prop-types';
 
-export default function InterviewerList(props) {
+function InterviewerList(props) {
   const {interviewers, onChange, value} = props;
   
   const interviewersData = interviewers.map(person => {
@@ -25,3 +26,9 @@ export default function InterviewerList(props) {
     </section>
   );
 }
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired
+};
+
+export default InterviewerList;
